0bebcbac29fd3b3f57129edfee391bd9f59fe189
AyoBakre <ayomidebakre@gmail.com>
2021-07-02 14:16:25 +0100

    charlesmaryayo

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ayomidescrumy/', include('ayomidescrumy.urls', namespace='ayomidescrumy'))
+    path('charlesmaryayo/', include('ayomidescrumy.urls', namespace='ayomidescrumy'))
 ]
